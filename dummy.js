504166ca1209bc9ef13ab788e2c0191afaa1d7e7
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2025-07-13 00:15:11 +0530

    made changes

---
+++ dummy.js
@@ -0,0 +1,18 @@
+const sampleArray = [
+    {
+        name: "Git",
+        module: "https://www.crio.do/learn/me/ME_GIT_BASICS/ME_GIT_BASICS_MODULE_BYTE1/",
+        cheetshit: "https://education.github.com/git-cheat-sheet-education.pdf",
+        history: "https://git-scm.com/book/en/v2/Getting-Started-A-Short-History-of-Git",
+        links:  {
+                    "Git Branches" : "https://www.atlassian.com/git/tutorials/using-branches",
+                    "What is the git folder?" : "https://stackoverflow.com/questions/29217859/what-is-the-git-folder/56145562",
+                    "Add all file to Repo" : "https://stackabuse.com/git-add-all-files-to-a-repo/",
+                    "Commit syntax" : "https://www.atlassian.com/git/tutorials/saving-changes/git-commit",
+                    "Commit message style guide" : "https://udacity.github.io/git-styleguide/",
+                    "Git Pull Explained": "https://www.freecodecamp.org/news/git-pull-explained",
+                    "Remote contains work that you don’t have locally error explained" : "https://blog.plover.com/prog/git-ff-error.html",
+
+                },
+    }
+];
